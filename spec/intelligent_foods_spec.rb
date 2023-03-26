3bc228b967f6612774a7302cf6266d0e60fa49bd
Chris Woodford <chris.woodford@gmail.com>
2023-03-26 15:39:27 -0700

    Set Up Linting Rules
    We want to use linting to ensure that all of our coding practices are
    consistent.
    
    This change addresses the need by:
    * Setting our Rubocop rules
    * Configuring Hound for Pull Requests

--- spec/intelligent_foods_spec.rb
+++ spec/intelligent_foods_spec.rb
@@ -2,10 +2,6 @@
 
 RSpec.describe IntelligentFoods do
   it "has a version number" do
-    expect(IntelligentFoods::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(IntelligentFoods::VERSION).not_to be_nil
   end
 end
